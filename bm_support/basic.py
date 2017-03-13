b9a95818a5c2e5b9146ea7f4647bc331fb9f6e6b
Alexander Belikov <abelikov@gmail.com>
2017-03-13 01:58:10 -0500

    fixes

--- bm_support/basic.py
+++ bm_support/basic.py
@@ -8,18 +8,18 @@ import datahelpers.plotting as dp
 def transform_df(dfi, statement_columns, action_column, claim_column,
                  index_name, aggregate_negs=True):
     df = dfi.copy()
-    print 'df shape:', df.shape, '. Type of', claim_column, \
-        'column', df[claim_column].dtype
+    print('df shape:', df.shape, '. Type of', claim_column, \
+        'column', df[claim_column].dtype)
     index_cs = statement_columns + [action_column]
     n_unique_triplets = df.drop_duplicates(index_cs).shape[0]
     if aggregate_negs:
         df = dc.aggregate_negatives_boolean_style(df, statement_columns,
                                                   action_column, claim_column)
-        print 'Number of unique (i_a, i_b, at) triplets after aggregation:', \
-            df.drop_duplicates(index_cs).shape[0]
-        print 'Fraction of unique (i_a, i_b, at) '\
+        print('Number of unique (i_a, i_b, at) triplets after aggregation:', \
+            df.drop_duplicates(index_cs).shape[0])
+        print('Fraction of unique (i_a, i_b, at) '\
             'triplets remaining after aggregation:', \
-            df.drop_duplicates(index_cs).shape[0]/float(n_unique_triplets)
+            df.drop_duplicates(index_cs).shape[0]/float(n_unique_triplets))
     # create new integer index for (i_a, i_b, at)
     df = dfto.get_multiplet_to_int_index(df, index_cs, index_name)
     return df
@@ -29,13 +29,13 @@ def try_impute_else_discard(df, impute_column, grouping_column):
     working_columns = [grouping_column, impute_column]
     df2 = df.copy()
     m = df2[impute_column].isnull()
-    print 'Number of NAs:', sum(m), 'values, total size', df2.shape[0]
+    print('Number of NAs:', sum(m), 'values, total size', df2.shape[0])
     df2[impute_column] = df[working_columns].groupby(grouping_column,
                                                      as_index=False).transform(lambda g:
                                                                     g.fillna(g.mean()))[impute_column]
     m = df2[impute_column].isnull()
-    print 'Could not impute', sum(m), \
-        'values (discarded), out of ', df2.shape[0]
+    print('Could not impute', sum(m), \
+        'values (discarded), out of ', df2.shape[0])
     return df2.loc[~m]
 
 
@@ -63,7 +63,7 @@ def split_data(df, c):
 
 
 def ks_and_hist(pair, k):
-    print stats.ks_2samp(pair[0][:, k], pair[1][:, k])
+    print(stats.ks_2samp(pair[0][:, k], pair[1][:, k]))
     pl_info = dp.plot_hist([pair[0][:, k], pair[1][:, k]],
                            approx_nbins=5, opacity=0.5,
                            ylog_axis=True, normed_flag=True)
