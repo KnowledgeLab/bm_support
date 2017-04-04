a0f28847101dcbd3b391a4f09c34b64cf70f2ccb
Alexander Belikov <abelikov@gmail.com>
2017-04-04 11:28:38 -0400

    feature: reporting

---
+++ bm_support/io/reporting.py
@@ -0,0 +1,17 @@
+def get_freq(report):
+    return report['freq'][0]
+
+
+def get_point_est_state(report):
+    root = report['posterior_info']['point']
+    return {k.split('_')[-1]: root[k][0] for k in root.keys() if k.startswith('pi')}
+
+
+def get_point_est_beta(report):
+    root = report['posterior_info']['point']
+    return {k: root[k][0] for k in root.keys() if not k.startswith('pi')}
+
+
+def get_false_point_est_state(report):
+    root = report['posterior_info']['point']
+    return {k: root[k] for k in root.keys() if k.startswith('pi') and not root[k][1][0]}
